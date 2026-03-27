401af8eb8e0301d42a4352109c4f4f2a690439a9
Josh <148491500+Pidgeyy@users.noreply.github.com>
2026-03-27 11:25:07 +0100

    Delete PROJET2 directory

--- PROJET2/campus-helpdesk/app/core/View.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/core/View.php
-
-class View {
-  public static function render($view, $data = array()) {
-    extract($data, EXTR_SKIP);
-    require __DIR__ . '/../views/layouts/main.php';
-  }
-}
--- PROJET2/campus-helpdesk/app/views/partials/alerts.php
+++
@@ -1,3 +0,0 @@
-<?php if (!empty($error)): ?>
-  <div class="alert alert-danger"><?php echo htmlspecialchars($error); ?></div>
-<?php endif; ?>
